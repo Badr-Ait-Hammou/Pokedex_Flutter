cdc6613732b7fa0dd5283f69c97581386b86ccdf
Badr-Ait-Hammou <badr.aithammou@emsi-edu.ma>
2023-03-28 17:12:27 +0000

    add api and getdata

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,10 +9,30 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var pokemonapi="https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  late List pokedex;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+
+      body: Center(
+        child:ElevatedButton(
+          child: Text('press Button'),
+          onPressed: (){
+            fetchPokemonData();
+          },
+        ),
+      ),
     );
+  }
+  void fetchPokemonData(){
+    var url = Uri.https('raw.githubusercontent.com', '/Biuni/PokemonGO-Pokedex/master/pokedex.json');
+    http.get(url).then((value) {
+      var decodedJsonData=jsonDecode(value.body);
+     // print(decodedJsonData);
+      pokedex=decodedJsonData['pokemon'];
+      print(pokedex);
+    });
+
   }
 }
